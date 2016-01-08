28ba8c509df648a9d94477d039dbb670dce2d779
vsoch <vsochat@stanford.edu>
2016-01-07 16:39:06 -0800

    adding MR image for viewer

--- treeci/js/concept.js
+++ treeci/js/concept.js
@@ -92,10 +92,11 @@ root = $.getJSON( "data/ri_" + image_id + ".json", function(root){
        $.each(root.image_list, function(index,image) {
           $('#scores_body').prepend('<tr><td><a href=neurovault.html?id=' + image  + '>'+ image + '</a></td><td><a href="http://www.neurovault.org/images/'+ image + ' target="_blank">View on NeuroVault</a></td></tr>');  
        });
+
+    $('#chart').dataTable();
+    } else {
+    $('#chart').remove();
     }
-
-
-   $('#chart').dataTable();
 
 }).error(function(){
     $('#chart').remove();
